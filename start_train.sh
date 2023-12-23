02474716c2cf1bd8f6b8ec91f19ba45cd66bbd63
ChrisKim <1515748548@qq.com>
2023-12-23 15:13:00 +0800

    [feat] 修改运行脚本

--- start_train.sh
+++ start_train.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py -net sam -mod sam_adpt -exp_name mydataset -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 1 -dataset mydataset -data_path ./data -val_freq 5 -vis 100
+python train.py -net sam -mod sam_adpt -exp_name mydataset -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -out_size 256 -b 1 -dataset mydataset -data_path ./data -val_freq 5 -vis 100
