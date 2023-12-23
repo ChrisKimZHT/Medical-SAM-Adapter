3b6899747c5aab89c5170b5a50ef8bacdff73e2d
ChrisKim <1515748548@qq.com>
2023-12-23 14:11:22 +0800

    [feat] 训练脚本

---
+++ start_train.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python train.py -net sam -mod sam_adpt -exp_name mydataset -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 1 -dataset mydataset -data_path ./data -val_freq 5 -vis 100
