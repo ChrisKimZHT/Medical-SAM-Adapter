1dd04e9a5046d4c1c8e8308e907b5b5836160e1c
ChrisKim <1515748548@qq.com>
2023-12-23 16:17:03 +0800

    [feat] 推理功能

--- function.py
+++ function.py
@@ -48,6 +48,7 @@ from monai.transforms import (
 
 
 import torch
+import torchvision.utils as vutils
 
 
 args = cfg.parse_args()
@@ -295,3 +296,114 @@ def validation_sam(args, val_loader, epoch, net: nn.Module, clean_dir=True):
         n_val = n_val * (imgsw.size(-1) // evl_ch)
 
     return tot/ n_val , tuple([a/n_val for a in mix_res])
+
+
+# predict function emplement only for my dataset
+def predict_sam(args, val_loader, epoch, net: nn.Module, clean_dir=True):
+    net.eval()
+
+    mask_type = torch.float32
+    n_val = len(val_loader)  # the number of batch
+    ave_res, mix_res = (0,0,0,0), (0,0,0,0)
+    rater_res = [(0,0,0,0) for _ in range(6)]
+    tot = 0
+    hard = 0
+    threshold = (0.1, 0.3, 0.5, 0.7, 0.9)
+    GPUdevice = torch.device('cuda:' + str(args.gpu_device))
+    device = GPUdevice
+
+    if args.thd:
+        lossfunc = DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
+    else:
+        lossfunc = criterion_G
+
+    with tqdm(total=n_val, desc='Predict round', unit='batch', leave=False) as pbar:
+        for ind, pack in enumerate(val_loader):
+            imgsw = pack['image'].to(dtype = torch.float32, device = GPUdevice)
+            masksw = pack['label'].to(dtype = torch.float32, device = GPUdevice)
+            # for k,v in pack['image_meta_dict'].items():
+            #     print(k)
+            if 'pt' not in pack:
+                imgsw, ptw, masksw = generate_click_prompt(imgsw, masksw)
+            else:
+                ptw = pack['pt']
+                point_labels = pack['p_label']
+            name = pack['image_meta_dict']['filename_or_obj']
+            
+            buoy = 0
+            if args.evl_chunk:
+                evl_ch = int(args.evl_chunk)
+            else:
+                evl_ch = int(imgsw.size(-1))
+
+            while (buoy + evl_ch) <= imgsw.size(-1):
+                if args.thd:
+                    pt = ptw[:,:,buoy: buoy + evl_ch]
+                else:
+                    pt = ptw
+
+                imgs = imgsw[...,buoy:buoy + evl_ch]
+                masks = masksw[...,buoy:buoy + evl_ch]
+                buoy += evl_ch
+
+                # if args.thd:
+                #     pt = rearrange(pt, 'b n d -> (b d) n')
+                #     imgs = rearrange(imgs, 'b c h w d -> (b d) c h w ')
+                #     masks = rearrange(masks, 'b c h w d -> (b d) c h w ')
+                #     imgs = imgs.repeat(1,3,1,1)
+                #     point_labels = torch.ones(imgs.size(0))
+
+                #     imgs = torchvision.transforms.Resize((args.image_size,args.image_size))(imgs)
+                #     masks = torchvision.transforms.Resize((args.out_size,args.out_size))(masks)
+                
+                showp = pt
+
+                mask_type = torch.float32
+                ind += 1
+                b_size,c,w,h = imgs.size()
+                longsize = w if w >=h else h
+
+                if point_labels[0] != -1:
+                    # point_coords = samtrans.ResizeLongestSide(longsize).apply_coords(pt, (h, w))
+                    point_coords = pt
+                    coords_torch = torch.as_tensor(point_coords, dtype=torch.float, device=GPUdevice)
+                    labels_torch = torch.as_tensor(point_labels, dtype=torch.int, device=GPUdevice)
+                    coords_torch, labels_torch = coords_torch[None, :, :], labels_torch[None, :]
+                    pt = (coords_torch, labels_torch)
+
+                '''init'''
+                if hard:
+                    true_mask_ave = (true_mask_ave > 0.5).float()
+                    #true_mask_ave = cons_tensor(true_mask_ave)
+                imgs = imgs.to(dtype = mask_type,device = GPUdevice)
+                
+                '''test'''
+                with torch.no_grad():
+                    imge= net.image_encoder(imgs)
+
+                    se, de = net.prompt_encoder(
+                        points=pt,
+                        boxes=None,
+                        masks=None,
+                    )
+
+                    pred, _ = net.mask_decoder(
+                        image_embeddings=imge,
+                        image_pe=net.prompt_encoder.get_dense_pe(),
+                        sparse_prompt_embeddings=se,
+                        dense_prompt_embeddings=de, 
+                        multimask_output=False,
+                    )
+
+                    #### change the size of pred ####
+                    pred = torchvision.transforms.Resize((640, 320))(pred)
+                    #################################
+                
+                    '''save predict'''
+                    namecat = 'predict'
+                    for na in name:
+                        img_name = na.split('/')[-1].split('.')[0]
+                        namecat = namecat + img_name
+                    vutils.save_image(pred, os.path.join(args.path_helper['sample_path'], namecat + '.jpg'))
+
+            pbar.update()
