06215e537c5506a105ffe1d86cb4169714f26f5f
ChrisKim <1515748548@qq.com>
2023-12-23 15:33:53 +0800

    [feat] 测试脚本

---
+++ start_val.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Input model weights path: "
+read -r weights
+
+python val.py -net sam -mod sam_adpt -exp_name mydataset -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -out_size 256 -b 1 -dataset mydataset -data_path ./data -val_freq 1 -vis 1 -weights $weights
